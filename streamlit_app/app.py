1e223eff11137c6ec4e85e327976feeae4c45661
jzanath <jonathan.e.zanath@sherwin.com>
2025-08-08 08:29:58 -0400

    added addtional pages

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -6,11 +6,15 @@ from database import functions as db
 def main() -> None:
     welcome = st.Page("pages/welcome.py", title="Welcome", icon=":material/home:", default=False)
     dashboardLanding = st.Page("pages/dashboardLanding.py", title="Home", icon=":material/home:", default=True)
+    siteBySite = st.Page(page="pages/siteBySite.py", title="Site By Site", icon=":material/factory:", default=False)
+    plantData = st.Page(page="pages/plantData.py", title="Plant Data", icon=":material/manufacturing:", default=False)
+
 
     pg = st.navigation(
         {
             "Dashboard Landing": [dashboardLanding],
-            "App": [welcome],
+            "Site By Site": [siteBySite],
+            "Plant Data": [plantData]
         }
     )
 
---
+++ streamlit_app/pages/plantData.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from database import functions as db
+
+st.write("This will be the site by site comparison page")
